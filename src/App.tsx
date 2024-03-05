c34e13a1d39224edf1b9ab258d8ce2d48e47b4db
Ahmed-aq <a.s.qayyum11@gmail.com>
2024-03-05 15:54:26 +0000

    Added the components for the basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Board from './components/Board';
 import './App.css';
+import TopBar from './components/TopBar';
+import Footer from './components/Footer';
 
 function App() {
-  return <main><Board /></main>
+  return <main>
+    <section>
+      <TopBar />
+      <Board />
+      <Footer />
+    </section>
+  </main>
 }
 
 export default App
