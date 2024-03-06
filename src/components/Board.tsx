0c260c090e5b83fd8705cf1536bb30d743372b5b
Ahmed-aq <a.s.qayyum11@gmail.com>
2024-03-06 22:46:40 +0000

    Added a utility function that returns the valid numbers that can go in a cell taking into account the current board state.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,9 +5,53 @@ interface Props {
     inputBoard: string[][];
 }
 
+// utility function to copy the board
 const copyBoard = (arr: string[][]) => {
     return arr.map(row => [...row]);
-} 
+}
+
+// utility funciton to return the valid numbers that can go in a certain cell
+const getValidNums = (i: number, j: number, curState: string[][]) => {
+    const used = new Set();
+
+    // add the numbers in the row
+    for (let k = 0 ; k < curState[i].length ; k++) {
+        const val = curState[i][k]; 
+        if (val !== '.') {
+            used.add(val);
+        }
+    }
+
+    // add the numbers in the column
+    for (let k = 0 ; k < curState.length ; k++) {
+        const val = curState[k][j];
+        if (val !== '.') {
+            used.add(val);
+        }
+    }
+
+    // add the numbers in the square
+    const startI = Math.floor(i / 3) * 3;
+    const startJ = Math.floor(j / 3) * 3;
+    for (let u = startI ; u < startI + 3 ; u++) {
+        for (let v = startJ ; v < startJ + 3 ; v++) {
+            const val = curState[u][v];
+            if (val !== '.') {
+                used.add(val);
+            }
+        }
+    }
+
+    const valid = [];
+    for (let k = 1 ; k < 10 ; k++) {
+        const val = k.toString();
+        if (!used.has(val)) {
+            valid.push(val);
+        }
+    }
+
+    return valid;
+}
 
 function Board({ inputBoard }: Props) {
     const [selectedCell, setSelectedCell] = useState("");
