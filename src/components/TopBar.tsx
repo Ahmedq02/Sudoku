a369814bf1d0849de77a7a0b1d5d4aa9e12ad4a9
Ahmed-aq <147751810+Ahmedq02@users.noreply.github.com>
2024-06-11 17:24:13 +0100

    Updated the designs for the main pages to make them more modern and sleek

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,9 +6,9 @@ function TopBar({ boardComplete }: Props) {
     return (
         <>
             {boardComplete ? (
-                <h1 className="top-bar complete">Congratulations!!!</h1>
+                <h1 className="top-bar complete">Congratulations</h1>
             ) : (
-                <h1 className="top-bar">Sudoku!</h1>
+                <h1 className="top-bar">Sudoku</h1>
             )}
         </>
     );
